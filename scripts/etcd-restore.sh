cf0db6c85eb7cee1f971adc6126f9ee1e5924fbe
Andrei Kvapil <kvapss@gmail.com>
2020-08-24 19:27:57 +0200

    convert scripts to generators

--- scripts/etcd-restore.sh
+++ scripts/etcd-restore.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 set -e
-if [ $# -lt 2 ]; then
-  echo "USAGE: $(basename $0) <statefulset> <file>"
+if [ $# -lt 3 ]; then
+  echo "USAGE: $(basename $0) <statefulset> <replicas> <file>"
   exit 1
 fi
-if [ ! -f $2 ]; then
-  echo "$2 is not a file"
+if [ ! -f $3 ]; then
+  echo "$3 is not a file"
   exit 1
 fi
 
-rand="$(env LC_ALL=C tr -dc a-z0-9 < /dev/urandom | head -c 8)"
+INITIAL_CLUSTER=$(for i in `seq 0 $(($2-1))`; do echo $1-$i=https://$1-$i.$1:2380; done | paste -s -d,)
 
+cat <<EOF
+# wait for all replicas
 kubectl rollout status -w sts "$1"
 
+# shutdown the cluster
 kubectl patch sts "$1" --patch "$(cat <<EOT
 apiVersion: apps/v1
 kind: StatefulSet
 spec:
   template:
-    metadata:
-      labels:
-        etcd-restore: "$rand"
     spec:
       containers:
       - name: etcd
@@ -31,23 +31,31 @@ spec:
       terminationGracePeriodSeconds: 0
 EOT
 )"
-trap 'kubectl rollout undo sts "$1"' EXIT HUP TERM
 
+# wait for all replicas
 kubectl rollout status -w sts "$1"
+EOF
 
-PODS=$(kubectl get pod -l "etcd-restore=$rand" --template='{{range .items}}{{.metadata.name}} {{end}}')
-INITIAL_CLUSTER=$(for i in $PODS; do echo $i=https://$i.$1:2380; done | paste -s -d,)
 
-for i in $PODS; do
-  set -x
-  kubectl cp "$2" "$i":/snapshot.db 
-  kubectl exec "$i" -- sh -c "set -x; rm -rf /default.etcd &&
-    etcdctl snapshot restore snapshot.db \
-      --data-dir=/default.etcd \
-      --name=\"$i\" \
-      --initial-cluster=\"$INITIAL_CLUSTER\" \
-      --initial-cluster-token=\"$1\" \
-      --initial-advertise-peer-urls=\"https://$i.$1:2380\"
-    "
-  kubectl exec "$i" -- sh -c 'rm -rf /var/lib/etcd/* && mv /default.etcd/* /var/lib/etcd/'
+for i in `seq 0 $(($2-1))`; do
+  cat <<EOT
+
+# restore $1-$i
+kubectl cp "$3" "$1-$i":/snapshot.db 
+kubectl exec "$1-$i" -- sh -c "set -x; rm -rf /default.etcd && etcdctl snapshot restore snapshot.db \\
+  --data-dir=/default.etcd \\
+  --name=\\"$1-$i\\" \\
+  --initial-cluster=\\"$INITIAL_CLUSTER\\" \\
+  --initial-cluster-token=\\"$1-$1\\" \\
+  --initial-advertise-peer-urls=\\"https://$1-$i.$1:2380\\"
+"
+kubectl exec "$1-$i" -- sh -c 'rm -rf /var/lib/etcd/* && mv /default.etcd/* /var/lib/etcd/'
+EOT
 done
+
+cat <<EOF
+
+# startup the cluster
+kubectl rollout undo sts "$1"
+EOF
+
