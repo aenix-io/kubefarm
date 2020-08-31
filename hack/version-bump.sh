9726800b3cc4153ddc289d797c29317c62b0fbe5
Andrei Kvapil <kvapss@gmail.com>
2020-08-31 14:52:19 +0200

    add: version-bump script

---
+++ hack/version-bump.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+EC=0
+
+version=$(git describe --tags --abbrev=0 | cut -c2-)
+echo "bumping version to $version"
+
+f=README.md
+sed -i "s/\(kubefarm --version\) [0-9]\+\.[0-9]\+\.[0-9]\+/\1 ${version}/" README.md
+git diff --exit-code "$f" && echo "$f not changed" && EC=1
+
+f=deploy/helm/kubefarm/Chart.yaml
+sed -i "s/\(^version:\) [0-9]\+\.[0-9]\+\.[0-9]\+/\1 ${version}/" "$f"
+git diff --exit-code "$f" && echo "$f not changed" && EC=1
+
+for f in \
+  examples/advanced_network/README.md \
+  examples/catchall/README.md \
+  examples/dualstack_network/README.md \
+  examples/generic/README.md
+do
+  sed -i "s/\(kubefarm --version\) [0-9]\+\.[0-9]\+\.[0-9]\+/\1 ${version}/" "$f"
+  git diff --exit-code "$f" && echo "$f not changed" && EC=1
+done
+
+echo
+
+kink_version=$(sed -n 's/version: //p' deploy/helm/kubefarm/charts/kubernetes/Chart.yaml)
+echo "bumping kink version to $kink_version"
+
+f=deploy/helm/kubefarm/Chart.yaml
+sed -i -z "s/\(name: kubernetes\n  version:\) [0-9]\+\.[0-9]\+\.[0-9]\+/\1 ${version}/" "$f"
+git diff --exit-code "$f" && echo "$f not changed" && EC=1
+
+if [ "$EC" != 0 ]; then
+  echo
+  echo "not all files were changed!"
+fi
+exit "$EC"
